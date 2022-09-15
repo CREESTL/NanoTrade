3748873f28f4fa8c7b9655e6c0e2304fc43389e0
IntegralTeam <sfxdx@sfxdx.com>
2022-09-15 23:00:30 +0300

    Fix Comments and Name

--- contracts/Dummy.sol
+++ contracts/Dummy.sol
@@ -4,11 +4,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @title A custom ERC20 contract used in the bridge
+/// @title A simple ERC20 used for test purposes
 contract Dummy is ERC20 {
 
-    /// @dev Creates an "empty" template token that will be cloned in the future
-    constructor() ERC20("", "") {}
+    constructor() ERC20("Dummy", "DMM") {}
 
     function giveTokens(address receiver, uint256 amount) external {
         super._mint(receiver, amount);
