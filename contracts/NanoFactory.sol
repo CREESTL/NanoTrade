0b09e0c0c42ee076de311f14401ca15a6d4ac9ca
IntegralTeam <sfxdx@sfxdx.com>
2022-09-19 19:12:02 +0300

    Add Admin Token Mint on ERC20 Token Creation

--- contracts/NanoFactory.sol
+++ contracts/NanoFactory.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ProducedToken.sol";
 import "./interfaces/INanoFactory.sol";
+import "./interfaces/INanoAdmin.sol";
+
 
 
 /// @title A factory of custom ERC20 tokens
@@ -13,14 +15,18 @@ contract NanoFactory is Ownable, INanoFactory {
 
     /// @dev Address of token to clone
     address private producedToken;
+    /// @dev Address of admin token to be minted to holders of cloned tokens
+    address private adminTokenAddress;
 
     /// @dev Create a new token template to copy and upgrade it later
-    constructor() {
+    /// @param _adminTokenAddress The address of admin token
+    constructor(address _adminTokenAddress) {
         producedToken = address(new ProducedToken());
+        adminTokenAddress = _adminTokenAddress;
     }
 
 
-    /// @notice Creates a new ERC20 token
+    /// @notice Creates a new ERC20 token and mints an admin token proving ownership
     /// @param name The name of the token
     /// @param symbol The symbol of the token
     /// @param decimals Number of decimals of the token
@@ -36,6 +42,9 @@ contract NanoFactory is Ownable, INanoFactory {
         // Copy the template functionality and create a new token (proxy pattern)
         address clonedToken = Clones.clone(producedToken);
         ProducedToken(clonedToken).initialize(name, symbol, decimals, mintable);
+
+        // Mint admin token to the creator of this ERC20 token
+        INanoAdmin(adminTokenAddress).mintWithERC20Address(msg.sender, clonedToken);
         
     }
 
