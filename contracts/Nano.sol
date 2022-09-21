aab575ad40fc8c8bc502febd65f8a64ef62ef18b
IntegralTeam <sfxdx@sfxdx.com>
2022-09-21 23:26:57 +0300

    Fix Weight Checking Logic

--- contracts/Nano.sol
+++ contracts/Nano.sol
@@ -44,18 +44,22 @@ contract Nano is INano, Ownable{
   ///        Can not be a zero address!
   /// @param distToken The address of the token that is to be disctributed as dividends
   ///        Zero address for native token (ether, wei)
-  /// @param amount The amount of distTokens to be distributed in total
   /// @param weight The amount of origTokens required to get a single distToken
-  function distributeDividendsWeighted(address[] memory receivers, address origToken, address distToken, uint256 amount, uint256 weight) external onlyOwner {
+  function distributeDividendsWeighted(address[] memory receivers, address origToken, address distToken, uint256 weight) external onlyOwner {
     require(receivers.length > 0, "Nano: no dividends receivers provided!");
     require(origToken != address(0), "Nano: original token can not have a zero address!");
     // It is impossible to give distTokens for zero origTokens
     require(weight >= 1, "Nano; weight is too low!");
-    // This function reverts if weight is incorrect. Use it to check.
-    calcMaxWeight(receivers, origToken, amount);
+    uint256 totalWeightedAmount = 0;
+    // This function reverts if weight is incorrect.
+    calcMinWeight(receivers, origToken, weight);
     for (uint256 i = 0; i < receivers.length; i++) {
       uint256 userBalance = IERC20(origToken).balanceOf(receivers[i]);
       uint256 weightedAmount = userBalance / weight;
+      totalWeightedAmount += weightedAmount;
+      // If total assumed amount of tokens to be distributed as dividends is higher than current contract's balance, than it it impossible to
+      // distribute dividends.
+      require(totalWeightedAmount <= IERC20(distToken).balanceOf(address(this)), "Nano: not enough dividend tokens to distribute with the provided weight!");
       if (distToken == address(0)) {
         // Native tokens (wei)
         (bool success, ) = receivers[i].call{value: weightedAmount}("");
@@ -66,17 +70,16 @@ contract Nano is INano, Ownable{
       }
     }
     
-    emit DividendsDistributed(distToken, amount);
+    emit DividendsDistributed(distToken, totalWeightedAmount);
   
   }
 
-  /// @notice Calculates the maximum currently allowed weight.
-  ///         Use this before distributing the dividends
+  /// @notice Calculates the minimum currently allowed weight.
+  ///         Weight used in distributing dividends should be equal/greater than this
   /// @param receivers The list of addresses of all receivers of dividends
   /// @param origToken The address of the token that is held by receivers
-  /// @param amount The amount of distTokens to be distributed in total
-  function calcMaxWeight(address[] memory receivers, address origToken, uint256 amount) public view returns(uint256) {
-    // Weight is the amount of origTokens required to get a single distToken
+  /// @param weight The amount of origTokens required to get a single distToken
+  function calcMinWeight(address[] memory receivers, address origToken, uint256 weight) public view returns(uint256) {
     // The highest balance of origTokens among receivers
     uint256 maxBalance = 0;
     for (uint256 i = 0; i < receivers.length; i++) {
@@ -85,10 +88,12 @@ contract Nano is INano, Ownable{
         maxBalance = singleBalance;
       }
     }
-    require(amount < maxBalance, "Nano: none of the receivers has enough tokens for current weight!");
-    // Max weight is the highes balance
-    uint256 maxWeight = maxBalance;
-    return maxWeight;
+    // If the minimum balance of origTokens among all receivers is less than weight - that means that
+    // none of the receivers will get any dividends
+    require(weight <= maxBalance, "Nano: none of the receivers has enough tokens for current weight!");
+    // Minimum weight is the highes balance
+    uint256 minWeight = maxBalance;
+    return minWeight;
   }
 
 
--- contracts/interfaces/INano.sol
+++ contracts/interfaces/INano.sol
@@ -23,17 +23,15 @@ interface INano {
   /// @param origToken The address of the token that is held by receivers
   /// @param distToken The address of the token that is to be disctributed as dividends
   ///        Zero address for native token (ether, wei)
-  /// @param amount The amount of distTokens to be distributed in total
-  // TODO which way should the weight work? 
   /// @param weight The amount of distTokens per single origToken on user's balance. Must be greater than or equal to 1!
-  function distributeDividendsWeighted(address[] memory receivers, address origToken, address distToken, uint256 amount, uint256 weight) external;
+  function distributeDividendsWeighted(address[] memory receivers, address origToken, address distToken, uint256 weight) external;
   
-  /// @notice Calculates the maximum currently allowed weight.
-  ///         Use this before distributing the dividends
+  /// @notice Calculates the minimum currently allowed weight.
+  ///         Weight used in distributing dividends should be equal/greater than this
   /// @param receivers The list of addresses of all receivers of dividends
   /// @param origToken The address of the token that is held by receivers
-  /// @param amount The amount of distTokens to be distributed in total
-  function calcMaxWeight(address[] memory receivers, address origToken, uint256 amount) external view returns(uint256);
+  /// @param weight The amount of origTokens required to get a single distToken
+  function calcMinWeight(address[] memory receivers, address origToken, uint256 weight) external view returns(uint256);
 
   /// @dev Indicates that dividends were distributed
   /// @param distToken The address of the token that is to be disctributed as dividends
