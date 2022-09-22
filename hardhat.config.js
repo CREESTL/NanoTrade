37ddc4a589cc68360923ff3e0e30eb03fa46c680
IntegralTeam <sfxdx@sfxdx.com>
2022-09-22 23:33:23 +0300

    Change Hardhat Configuration. Add Chains.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,58 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const {
+        ETHERSCAN_API_KEY,
+        ACC_PRIVATE_KEY,
+        INFURA_API_KEY
+    } = process.env;
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    // Ethereum Goerli testnet
+    // TODO Use Alchemy here
+    goerli: {
+      url: `https://goerli.prylabs.net`,
+      accounts: [ACC_PRIVATE_KEY]
+    },
+    // Ethereum mainnet
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ACC_PRIVATE_KEY]
+    }
+  },
+  mocha: {
+    timeout: 20000000000
+  },
+  etherscan: {
+    apiKey: {
+      ethereum: ETHERSCAN_API_KEY,
+      goerli: ETHERSCAN_API_KEY
+    }
+  },
+  skipFiles: ["node_modules"],
+    gasReporter: {
+        enabled: true,
+        url: "http://localhost:8545"
+    },
 };
