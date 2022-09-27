dbbc12b62f2d247806ee541e2342887223df3028
IntegralTeam <sfxdx@sfxdx.com>
2022-09-27 13:55:05 +0300

    Remove Using Clones

--- contracts/NanoFactory.sol
+++ contracts/NanoFactory.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NanoProducedToken.sol";
 import "./interfaces/INanoFactory.sol";
 import "./interfaces/INanoAdmin.sol";
@@ -12,21 +10,7 @@ import "./interfaces/INanoAdmin.sol";
 
 
 /// @title A factory of custom ERC20 tokens
-contract NanoFactory is Ownable, INanoFactory, IERC721Receiver {
-
-    /// @dev Address of token to clone
-    address private _producedToken;
-
-    /// @dev Create a new token template to copy and upgrade it later
-    constructor() {
-        _producedToken = address(new NanoProducedToken(address(this)));
-    }
-
-    /// @dev Returns the address of the token that is produced by the factory
-    /// @return The address of the token that is produced by the factory
-    function producedToken() external view onlyOwner returns(address) {
-        return _producedToken;
-    }
+contract NanoFactory is INanoFactory, IERC721Receiver {
 
     /// @dev Allows this factory contract to receive admin tokens to manage controlled tokens
     function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) public returns (bytes4) {
@@ -50,21 +34,17 @@ contract NanoFactory is Ownable, INanoFactory, IERC721Receiver {
         address adminToken_
     ) external {
 
-        // Copy the template functionality and create a new token (proxy pattern)
-        address clonedToken = Clones.clone(_producedToken);
-
-        // Initialize a new token
-        NanoProducedToken(clonedToken).initialize(
+        NanoProducedToken newToken = new NanoProducedToken(
             name, 
-            symbol, 
-            decimals, 
-            mintable, 
+            symbol,
+            decimals,
+            mintable,
             maxTotalSupply,
             adminToken_
-        );
-        
+        );            
+
         // Mint admin token to the creator of this ERC20 token
-        INanoAdmin(adminToken_).mintWithERC20Address(msg.sender, clonedToken);
+        INanoAdmin(adminToken_).mintWithERC20Address(msg.sender, address(newToken));
         
     }
 
