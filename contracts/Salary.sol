fe87f1392fcaf4b7204eb8248b060602f1699718
Danila Korneev <dkorneev@sfxdx.com>
2022-12-13 18:03:39 +0700

    filling methods

--- contracts/Salary.sol
+++ contracts/Salary.sol
@@ -2,9 +2,13 @@
 
 pragma solidity ^0.8.9;
 
-contract Salary {
 
-    struct Salary {
+import "./interfaces/IBentureAdmin.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+//include IERC20
+contract Salary {
+    struct SalaryInfo {
         uint256 id;
         uint256 periodDuration;
         uint256 amountOfPeriods;
@@ -13,36 +17,187 @@ contract Salary {
         uint256 totalTokenAmount;
         uint256 tokensAmountPerPeriod;
         uint256 lastWithdrawalTime;
+        address employer;
+        address employee;
     }
+
+    uint256 private lastSalaryId;
+    address private bentureAdminToken;
 
     mapping(address => string) private names;
     mapping(address => address[]) private adminToEmployees;
     mapping(address => address[]) private employeeToAdmins;
-    mapping(uint256 => Salary) private salaryById;
-    mapping(address => Salary) private employeeToSalary;
-    mapping(Salary.id => address) private salaryIdToEmployee;
+    mapping(uint256 => SalaryInfo) private salaryById;
+    mapping(address => SalaryInfo[]) private employeeToSalaries;
+    //mapping(uint256 => address) private salaryIdToEmployee;
+    //mapping(uint256 => address) private salaryIdToEmployer;
 
-    event EmployeeAdded(uint256 indexed employeeAddress, uint256 indexed adminAddress);
-    event EmployeeRemoved(uint256 indexed employeeAddress, uint256 indexed adminAddress);
-    event EmployeeNameChanged(uint256 indexed employeeAddress, string indexed name);
-    event EmployeeNameRemoved(uint256 indexed employeeAddress, string indexed name);
-    event EmployeeSalaryAdded(uint256 indexed employeeAddress, Salary indexed salary);
-    event EmployeeSalaryRemoved(uint256 indexed employeeAddress, Salary indexed salary);
+    event EmployeeAdded(address indexed employeeAddress, address indexed adminAddress);
+    event EmployeeRemoved(address indexed employeeAddress, address indexed adminAddress);
+    event EmployeeNameChanged(address indexed employeeAddress, string indexed name);
+    event EmployeeNameRemoved(address indexed employeeAddress);
+    event EmployeeSalaryAdded(address indexed employeeAddress, address indexed adminAddress, SalaryInfo indexed salary);
+    event EmployeeSalaryRemoved(address indexed employeeAddress, address indexed adminAddress, SalaryInfo indexed salary);
 
-    function getNameOfEmployee(address employeeAddress) external returns(string name);
-    function getEmployeesByAdmin(address adminAddress) external returns(address[] employees);
-    function checkIfUserIsEmployeeOfAdmin(address adminAddress, address employeeAddress) external returns(bool isEmployee);
-    function getAdminsByEmployee(address employeeAddress) external returns(address[] admins);
-    function checkIfUserIsAdminOfEmployee(address employeeAddress, address adminAddress) external returns(bool isAdmin);
-    function getSalariesByEmployee(address employeeAddress) external returns(Salary[] salaries);
-    function getEmployeeBySalary(address salaryId) external returns(address employee);
-    function getSalaryById(address salaryId) external returns(Salary salary);
+    modifier onlyAdmin() {
+        IBentureAdmin(bentureAdminToken).checkOwner(msg.sender);
+        _;
+    }
 
-    function addEmployee(address _employeeAddress) external onlyAdmin;
-    function removeEmployee(address _employeeAddress) external onlyAdmin;
-    function addNameToEmployee(address _employeeAddress, string _name) external onlyAdmin;
-    function removeNameFromEmployee(address _employeeAddress) external onlyAdmin;
-    function addSalaryToEmployee(address _employeeAddress, Salary _salary) external onlyAdmin;
-    function removeSalaryFromEmployee(address _employeeAddress, uint256 _salaryId) external onlyAdmin;
-    function withdrawSalary(uint256 [] _salaryId) external;
+    constructor(address tokenAddress) {
+       bentureAdminToken = tokenAddress;
+    }
+
+    function getNameOfEmployee(address employeeAddress) external returns(string memory name) {
+        return names[employeeAddress];
+    }
+
+    function getEmployeesByAdmin(address adminAddress) public view returns(address[] memory employees) {
+        return adminToEmployees[adminAddress];
+    }
+
+    function checkIfUserIsEmployeeOfAdmin(address adminAddress, address employeeAddress) public view returns(bool isEmployee) {
+        bool isEmployee = false;
+        for (uint256 i = 0; i <= adminToEmployees[adminAddress].length - 1; i++) {
+            if (adminToEmployees[adminAddress][i] == employeeAddress) {
+                isEmployee = true;
+                break;
+            }
+        }
+        return isEmployee;
+    }
+
+    function getAdminsByEmployee(address employeeAddress) external view returns(address[] memory admins) {
+        return employeeToAdmins[employeeAddress];
+    }
+
+    function checkIfUserIsAdminOfEmployee(address employeeAddress, address adminAddress) public view returns(bool isAdmin) {
+        bool isAdmin = false;
+        for (uint256 i = 0; i <= employeeToAdmins[employeeAddress].length - 1; i++) {
+            if (employeeToAdmins[employeeAddress][i] == adminAddress) {
+                isAdmin = true;
+                break;
+            }
+        }
+        return isAdmin;
+    }
+
+    function getSalariesByEmployee(address employeeAddress) public view returns(SalaryInfo[] memory salaries) {
+        return employeeToSalaries[employeeAddress];
+    }
+
+    /* function getEmployeeBySalaryId(uint256 salaryId) public view returns(address employee) {
+        return salaryIdToEmployee[salaryId];
+    } */
+
+    function getSalaryById(uint256 salaryId) public view returns(SalaryInfo memory salary) {
+        return salaryById[salaryId];
+    }
+
+    function addEmployee(address employeeAddress) external onlyAdmin {
+        require(!checkIfUserIsAdminOfEmployee(employeeAddress, msg.sender), "User already is employee");
+        adminToEmployees[msg.sender].push(employeeAddress);
+        employeeToAdmins[employeeAddress].push(msg.sender);
+        emit EmployeeAdded(employeeAddress, msg.sender);
+    }
+
+    function removeEmployee(address employeeAddress) external onlyAdmin {
+        for (uint256 i = 0; employeeToSalaries[employeeAddress].length - 1; i++) {
+            if(employeeToSalaries[employeeAddress][i].employer == msg.sender) {
+                removeSalaryFromEmployee(employeeAddress, employeeToSalaries[employeeAddress][i].salaryId); 
+            }
+        }
+
+        uint256 lastIndex = adminToEmployees[msg.sender].length - 1;
+        for (uint256 i = 0; i <= lastIndex; i++) {
+            if (adminToEmployees[msg.sender][i] == employeeAddress) {
+                adminToEmployees[msg.sender][i] = adminToEmployees[msg.sender][lastIndex];
+                adminToEmployees[msg.sender].pop();
+                break;
+            }
+        }
+
+        lastIndex = employeeToAdmins[employeeAddress].length - 1;
+        for (uint256 i = 0; i <= lastIndex; i++) {
+            if (employeeToAdmins[employeeAddress][i] == msg.sender) {
+                employeeToAdmins[employeeAddress][i] = employeeToAdmins[employeeAddress][lastIndex];
+                employeeToAdmins[employeeAddress].pop();
+                emit EmployeeRemoved(employeeAddress, msg.sender);
+                break;
+            }
+        }
+    }
+
+    function setNameToEmployee(address employeeAddress, string memory name) external onlyAdmin {
+        require(checkIfUserIsAdminOfEmployee(employeeAddress, msg.sender), "Not allowed to set name");
+        names[employeeAddress] = name;
+        emit EmployeeNameChanged(employeeAddress, name);
+    }
+
+    function removeNameFromEmployee(address employeeAddress) external onlyAdmin {
+        require(checkIfUserIsAdminOfEmployee(employeeAddress, msg.sender), "Not allowed to remove name");
+        delete names[employeeAddress];
+        emit EmployeeNameRemoved(employeeAddress);
+    }
+
+    function addSalaryToEmployee(
+        address employeeAddress, 
+        uint256 periodDuration,
+        uint256 amountOfPeriods,
+        address tokenAddress,
+        uint256 totalTokenAmount,
+        uint256 tokensAmountPerPeriod
+    ) external onlyAdmin {
+        require(IERC20(tokenAddress).allowance(msg.sender, employeeAddress) >= totalTokenAmount, "Not enough tokens allowed");
+        SalaryInfo memory _salary;
+        lastSalaryId++;
+        _salary.id = lastSalaryId;
+        _salary.periodDuration = periodDuration;
+        _salary.amountOfPeriods = amountOfPeriods;
+        _salary.amountOfWithdrawals = 0;
+        _salary.tokenAddress = tokenAddress;
+        _salary.totalTokenAmount = totalTokenAmount;
+        _salary.tokensAmountPerPeriod = tokensAmountPerPeriod;
+        _salary.lastWithdrawalTime = block.timestamp;
+        _salary.employer = msg.sender;
+        _salary.employee= employeeAddress;
+        employeeToSalaries[employeeAddress].push(_salary);
+        salaryById[_salary.id] = _salary;
+        //salaryIdToEmployee[employeeAddress] = _salary.id;
+        //salaryIdToEmployer[msg.sender] = _salary.id;
+        emit EmployeeSalaryAdded(employeeAddress, msg.sender, _salary);
+    }
+
+/// Проверить что зарплата принадлежит этому пользователю
+    function removeSalaryFromEmployee(address employeeAddress, uint256 salaryId) external onlyAdmin {
+        require(!checkIfUserIsAdminOfEmployee(employeeAddress, msg.sender), "Not an admin for employee");
+        require(salaryById[salaryId].employer == msg.sender, "Not an admin of salary")
+        require(salaryById[salaryId].employee == employeeAddress, "Not an employee of salary")
+        //require(salaryIdToEmployer[salaryId] == msg.sender, "Not an admin of salary");
+
+        SalaryInfo _salary = salaryById[salaryId];
+        //(tokensAmountPerPeriod / periodDuration) * (block.timestamp - _salary.lastWithdrawalTime)
+        if block.timestamp >= 
+        uint256 amountToPay = (tokensAmountPerPeriod / periodDuration) * (block.timestamp - _salary.lastWithdrawalTime)
+        amountOfWithdrawals < amountOfPeriods;
+        //выплатить остаток зп
+
+
+        uint256 lastIndex = employeeToSalaries[employeeAddress].length - 1;
+        for (uint256 i = 0; i <= lastIndex; i++) {
+            if (employeeToSalaries[employeeAddress][i].id == salaryId) {
+                employeeToSalaries[employeeAddress][i] = employeeToSalaries[employeeAddress][lastIndex];
+                employeeToSalaries[employeeAddress].pop();
+                break;
+            }
+        }
+    }
+
+//array?
+    function withdrawSalary(uint256 salaryId) external {
+        //require(salaryIdToEmployee[salaryId] == msg.sender, "Not employee for this salary");
+        require(salaryIdToEmployee[salaryId] == msg.sender, "Not employee for this salary");
+        SalaryInfo _salary = salaryById[salaryId];
+        if (_salary.lastWithdrawalTime >=)
+    }
 }
--- contracts/interfaces/ISalary.sol
+++ contracts/interfaces/ISalary.sol
@@ -4,6 +4,17 @@ pragma solidity ^0.8.9;
 
 /// @title An interface of a Salary contract.
 interface ISalary {
+    struct SalaryInfo {
+        uint256 id;
+        uint256 periodDuration;
+        uint256 amountOfPeriods;
+        uint256 amountOfWithdrawals;
+        address tokenAddress;
+        uint256 totalTokenAmount;
+        uint256 tokensAmountPerPeriod;
+        uint256 lastWithdrawalTime;
+    }
+
     /// @dev Indicates that a new employee was added.
     event EmployeeAdded(uint256 indexed employeeAddress, uint256 indexed adminAddress);
 
@@ -17,20 +28,20 @@ interface ISalary {
     event EmployeeNameRemoved(uint256 indexed employeeAddress, string indexed name);
 
     /// @dev Indicates that an employee's salary was added.
-    event EmployeeSalaryAdded(uint256 indexed employeeAddress, Salary indexed salary);
+    event EmployeeSalaryAdded(uint256 indexed employeeAddress, SalaryInfo indexed salary);
 
     /// @dev Indicates that an employee's salary was removed.
-    event EmployeeSalaryRemoved(uint256 indexed employeeAddress, Salary indexed salary);
+    event EmployeeSalaryRemoved(uint256 indexed employeeAddress, SalaryInfo indexed salary);
 
     /// @notice Returns the name of employee.
     /// @param employeeAddress Address of employee.
     /// @return name The name of employee.
-    function getNameOfEmployee(address employeeAddress) external returns(string name);
+    function getNameOfEmployee(address employeeAddress) external returns(string memory name);
 
     /// @notice Returns the array of employees of admin.
     /// @param adminAddress Address of admin.
     /// @return employees The array of employees of admin.
-    function getEmployeesByAdmin(address adminAddress) external returns(address[] employees);
+    function getEmployeesByAdmin(address adminAddress) external returns(address[] memory employees);
 
     /// @notice Returns true if user if employee for admin and False if not.
     /// @param adminAddress Address of admin.
@@ -41,28 +52,28 @@ interface ISalary {
     /// @notice Returns the array of admins of employee.
     /// @param employeeAddress Address of employee.
     /// @return admins The array of admins of employee.
-    function getAdminsByEmployee(address employeeAddress) external returns(address[] admins);
+    function getAdminsByEmployee(address employeeAddress) external returns(address[] memory admins);
 
     /// @notice Returns true if user if admin for employee and False if not.
     /// @param employeeAddress Address of employee.
     /// @param adminAddress Address of admin.
-    /// @return isEmployee True if user if admin for employee. False if not.
+    /// @return isAdmin True if user if admin for employee. False if not.
     function checkIfUserIsAdminOfEmployee(address employeeAddress, address adminAddress) external returns(bool isAdmin);
 
     /// @notice Returns array of salaries of employee.
     /// @param employeeAddress Address of employee.
     /// @return salaries Array of salaries of employee.
-    function getSalariesByEmployee(address employeeAddress) external returns(Salary[] salaries);
+    function getSalariesByEmployee(address employeeAddress) external returns(SalaryInfo[] memory salaries);
 
     /// @notice Returns employee by salary ID.
-    /// @param salatyId Id of Salary.
+    /// @param salaryId Id of SalaryInfo.
     /// @return employee Employee address.
     function getEmployeeBySalary(address salaryId) external returns(address employee);
 
     /// @notice Returns salary by ID.
-    /// @param salatyId Id of Salary.
-    /// @return salary Salary by ID.
-    function getSalaryById(address salaryId) external returns(Salary salary);
+    /// @param salaryId Id of SalaryInfo.
+    /// @return salary SalaryInfo by ID.
+    function getSalaryById(address salaryId) external returns(SalaryInfo memory salary);
 
     /// @notice Adds new employee.
     /// @param employeeAddress Address of employee.
@@ -78,7 +89,7 @@ interface ISalary {
     /// @param employeeAddress Address of employee.
     /// @param name New name of employee.
     /// @dev Only admin can call this method.
-    function addNameToEmployee(address employeeAddress, string name) external;
+    function addNameToEmployee(address employeeAddress, string memory name) external;
 
     /// @notice Removes name from employee.
     /// @param employeeAddress Address of employee.
@@ -88,7 +99,7 @@ interface ISalary {
     /// @notice Adds salary to employee.
     /// @param employeeAddress Address of employee.
     /// @dev Only admin can call this method.
-    function addSalaryToEmployee(address employeeAddress, Salary salary) external;
+    function addSalaryToEmployee(address employeeAddress, SalaryInfo memory salary) external;
 
     /// @notice Removes salary from employee.
     /// @param employeeAddress Address of employee.
@@ -99,5 +110,5 @@ interface ISalary {
     /// @notice Withdraws employee's salary.
     /// @param salaryId IDs of employee salaries.
     /// @dev Anyone can call this method. No restrictions.
-    function withdrawSalary(uint256 [] salaryId) external;
+    function withdrawSalary(uint256[]  memory salaryId) external;
 }
